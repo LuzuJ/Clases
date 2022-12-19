677523c4f33f12500d070eb635cba8dd38184ec6
nuestro usuario <nuestro email>
2022-12-18 19:51:25 -0500

    Hola

--- src/pkPoliEstudiante/Calendario.java
+++ src/pkPoliEstudiante/Calendario.java
@@ -17,27 +17,14 @@ public class Calendario {
             for (int c = 0; c < calendar[f].length; c++)
                 calendar[f][c] = " ";
 
-        calendar[0][0]= " ";
-        calendar[0][1]= "Lun";
-        calendar[0][2]= "Mar";
-        calendar[0][3]= "Mie";
-        calendar[0][4]= "Jue";
-        calendar[0][5]= "Vie";
-        calendar[0][6]= "Sab";
-        calendar[0][7]= "Dom";
+        calendar[0][0]= " "; calendar[0][1]= "Lun"; calendar[0][2]= "Mar";
+        calendar[0][3]= "Mie"; calendar[0][4]= "Jue"; calendar[0][5]= "Vie";
+        calendar[0][6]= "Sab"; calendar[0][7]= "Dom";
 
-        calendar[1][0] = "5-6";
-        calendar[2][0] = "6-7";
-        calendar[3][0] = "7-8";
-        calendar[4][0] = "8-9";
-        calendar[5][0] = "10-11";
-        calendar[6][0] = "11-12";
-        calendar[7][0] = "12-13";
-        calendar[8][0] = "13-14";
-        calendar[9][0] = "15-16";
-        calendar[10][0] = "16-17";
-        calendar[11][0] = "17-18";
-        calendar[12][0] = "18-19";
+        calendar[1][0] = "5-6"; calendar[2][0] = "6-7"; calendar[3][0] = "7-8";
+        calendar[4][0] = "8-9"; calendar[5][0] = "10-11"; calendar[6][0] = "11-12";
+        calendar[7][0] = "12-13"; calendar[8][0] = "13-14"; calendar[9][0] = "15-16";
+        calendar[10][0] = "16-17"; calendar[11][0] = "17-18"; calendar[12][0] = "18-19";
 
        
         for (int x = 0; x < calendar.length; x++) {
